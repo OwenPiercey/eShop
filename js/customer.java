b111911b8a7f740be7cc9d237ab218c387590040
OwenPiercey <me@owenpiercey.ca>
2018-09-20 21:48:49 -0300

    now

---
+++ js/customer.java
@@ -0,0 +1,8 @@
+/*
+This class file will contain all the information of the current user
+pulled from the database.java class
+*/
+
+class customer{
+  
+}
---
+++ js/database.java
@@ -0,0 +1,8 @@
+/*
+  This databse class connects to a goDaddy sql database.
+  Username is OwenPiercey002 for godaddy
+*/
+
+class database{
+
+}
